ced4dbfa4e0fd93b3c6386c436d577ce173752c8
Charles-Hong520 <chong039@ucr.edu>
2021-10-28 14:38:09 -0700

    copy header code

--- source/main.c
+++ source/main.c
@@ -12,10 +12,36 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
 
-    /* Insert your solution below */
+
+void set_PWM(double frequency) {
+    static double current_frequency;
+    if(frequency != current_frequency) {
+        if(!frequency) {TCCR3B &= 0x08;}
+        else {TCCR3B |= 0x03;}
+
+        if(frequency < 0.954) {OCR3A = 0xFFFF;}
+        else if(frequency > 31250) {OCR3A = 0x0000;}
+        else {OCR3A = (short)(8000000/(128*frequency))-1;}
+        TCNT3=0;
+        current_frequency = frequency;
+    }
+}
+
+void PWM_on() {
+    TCCR3A = (1<<COM3A0);
+    TCCR3B = (1<<WGM32) | (1<CS31) | (1<<CS30);
+    set_PWM(0);
+}
+
+void PWM_off() {
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+double freq[] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+//                 C       D       E       F       G       A       B       C5
+int main(void) {
+
     while (1) {
 
     }
